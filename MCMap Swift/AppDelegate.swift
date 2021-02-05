44cbea74f6383f489b997c50ae66aa7a3a84cd91
C.W. Betts <computers57@hotmail.com>
2021-02-05 14:17:03 -0700

    Begin re-write of MCMap Live: It was made by someone not familiar with Objective-C.

---
+++ MCMap Swift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MCMap Swift
+//
+//  Created by C.W. Betts on 2/5/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
